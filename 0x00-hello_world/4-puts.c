0370035cf577ec811d21f13148bee12021ff151f
mikehailu7 <mikehailu7@gmail.com>
2023-02-16 03:49:25 -0800

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
-#include<stdio.h>
-
+#include <stdio.h>
 
 /**
  * main - prints "Programming is like building a multilingual
- *  puzzle, followed by a newline.
- *  Return: Always 0.
+ * puzzle, followed by a newline.
+ * Return: Always 0.
  */
 
 int main(void)
