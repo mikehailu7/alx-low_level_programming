3147a43e23bdc6703bc88203e843d491dd4797d2
mikehailu7 <mikehailu7@gmail.com>
2023-02-16 04:22:23 -0800

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
-/** main 
- * is
- * the most 
- */
+/**
+ * main - prints "with proper grammer, but the outcome is 
+ * a piece of art," followed newline
+ * Return: Always 0. 
+*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
