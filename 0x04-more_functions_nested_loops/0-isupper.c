de7f8642d50a60c0aa18bbb70eeb06902d8dc1e5
mikehailu7 <mikehailu7@gmail.com>
2023-02-23 03:42:08 -0800

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,9 +1,17 @@
 #include "main.h"
-int _isdigit(int c)
+
+/**
+ * _isupper - uppercase letters
+ * @c: character to check
+ *
+ * Return: 0 or 1
+ */
+int _isupper(int c)
+
 {
-	if (c >= '0' && c <= '9')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
-}
 
+}
