2ba22d3bd0a6a10aa9ef69606bab29c6bb2f036a
mikehailu7 <mikehailu7@gmail.com>
2023-02-26 21:41:55 -0800

    9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * @dest: char to check
+ * @src: char to check
+ * Return: 0 is success
+ * mikias 
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int a;
+
+	for (a = 0; src[a] != '\0'; a++)
+		dest[a] = src[a];
+	dest[a] = '\0';
+	return (dest);
+}
