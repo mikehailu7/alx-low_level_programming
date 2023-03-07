874f339ef7cc8e91273f76e1e1ce2aca295b6942
mikehailu7 <mikehailu7@gmail.com>
2023-03-07 05:58:44 -0800

    2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,30 +1,23 @@
 #include "main.h"
 
 /**
- * _strspn - a function that gets the length of a prefix substring
- * @s: the initial segment of
- * @accept: which consist only of bytes from
- * Return: the number of bytes
- * mikias
+ * _strchr - locates a character in a string
+ * @c: occurrence of the character
+ * @s: in the string
+ * Return: a pointer to the first occurrence of the character
  */
 
-unsigned int _strspn(char *s, char *accept)
+char *_strchr(char *s, char c)
 {
-	int z = 0, x, y;
-
-	for (x = 0; s[x] != '\0'; x++)
+	while (*s)
 	{
-		if (s[x] != 32)
-		{
-			for (y = 0; accept[y] != '\0'; y++)
-			{
-				if (s[x] == accept[y])
-					z++;
-			}
-		}
+		if (*s != c)
+			s++;
 		else
-			return (z);
+			return (s);
 	}
-		return (z);
+	if (c == '\0')
+		return (s);
 
+	return (NULL);
 }
