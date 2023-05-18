73bff19193151091f913e20310af1134057bf670
mikehailu7 <mikehailu7@gmail.com>
2023-05-18 05:51:37 -0700

    D-lists

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * freelist
+ * free_dlistint: this fucntion will free a list
+ * @head: This pointer will point to the head
+ * Autor: MikiasHailu
+ * Return: void
+ */
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *nxt;
+	while (head != NULL)
+	{
+		nxt = head->nxt;
+		free(head);
+		head = nxt;
+	}
+}
