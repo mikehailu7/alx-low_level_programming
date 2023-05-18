39e82b9fd07707bb48bd270e0dfc9b4405be30a6
mikehailu7 <mikehailu7@gmail.com>
2023-05-18 05:01:00 -0700

    D-lists

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -7,22 +7,26 @@
  * @n: this is the node
  * @head: double pointer to the beginning of the linked list
  * Author: mikiashailu
- * Return: This will return p if it is right.
+ * Return: This will return n if it is right.
  */
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
+	dlistint_t *h;
 	dlistint_t *new;
-
-	if (head == NULL)
-		return (NULL);
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
 		return (NULL);
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
+	h = *head;
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+	new->next = h;
+	if (h != NULL)
+		h->prev = new;
 	*head = new;
-	if (new->next != NULL)
-		(new->next)->prev = new;
 	return (new);
 }
