f8153d3a0f5dd79ddcd1826af45758c8c61d8b42
mikehailu7 <mikehailu7@gmail.com>
2023-05-18 04:20:14 -0700

    D-lists

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -16,7 +16,7 @@ typedef struct dlistint_s
     struct dlistint_s *prev;
 } dlistint_t;
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int p);
-size_t print_dlistint(const dlistint_t *h);
+size_t p_dlistint(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int p);
 size_t dlistint_len(const dlistint_t *h);
 void free_dlistint(dlistint_t *head);
