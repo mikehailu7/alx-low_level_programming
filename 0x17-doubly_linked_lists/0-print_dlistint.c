1b6d0c8bb9aa7fbccbb860d8a73dd75bbc10ec0d
mikehailu7 <mikehailu7@gmail.com>
2023-05-18 03:47:43 -0700

    D-lists

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -9,11 +9,11 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	size_t i;
-	for (i = 0; h != NULL; i++)
+	size_t m;
+	for (m = 0; h != NULL; m++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (i);
+	return (m);
 }
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,8 @@
 #ifndef LIST_H
 #define LIST_H
+
 #include <stddef.h>
 /**
- * project: double linked list.
  * struct dlistint_s: This represents a doubly linked list.
  * @next: points to the next node.
  * @prev: points to the previous node.
@@ -15,7 +15,6 @@ typedef struct dlistint_s
     struct dlistint_s *next;
     struct dlistint_s *prev;
 } dlistint_t;
-
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
