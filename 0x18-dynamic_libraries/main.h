f471bfc15f850a64c6205465eae7fb3327893ec7
mikehailu7 <mikehailu7@gmail.com>
2023-05-22 03:59:25 -0700

    dynamic

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -3,25 +3,25 @@
 /*
  * all the function calls
  * Author: mikias hailu
- * C dynamic libraries
+ * Cdynamic libraries
 */
-int _atoi(char *s);
+int _atoi(char *m);
 int _strlen(char *s);
-int _isupper(int c);
-char *_strchr(char *s, char c);
-int _islower(int c);
-int _isalpha(int c);
-int _abs(int n);
-char *_memcpy(char *dest, char *src, unsigned int n);
-int _putchar(char c);
+int _isupper(int m);
+char *_strchr(char *s, char m);
+int _islower(int m);
+int _isalpha(int m);
+int _abs(int d);
+char *_memcpy(char *dest, char *src, unsigned int d);
+int _putchar(char m);
 char *_strncat(char *dest, char *src, int n);
 char *_strstr(char *haystack, char *needle);
-int _isdigit(int c);
-void _puts(char *s);
+int _isdigit(int m);
+void _puts(char *n);
 char *_strcat(char *dest, char *src);
-char *_strncpy(char *dest, char *src, int n);
+char *_strncpy(char *dest,  int d, char *src);
 int _strcmp(char *s1, char *s2);
-char *_memset(char *s, char b, unsigned int n);
+char *_memset( unsigned int n, char *s, char b);
 char *_strncat(char *dest, char *src, int n);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
