a491b458fee6fdf56977c7b1c90b46c22c155157
mikehailu7 <mikehailu7@gmail.com>
2023-07-06 08:39:06 -0700

    hash

---
+++ 0x1A-hash_tables/hash_tables.h
@@ -0,0 +1,73 @@
+#ifndef _HASH_TABLES_H_
+#define _HASH_TABLES_H_
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct shash_table_s: This structure represents the data structure of the sorted hash table.
+ * Author: MikiasHailu
+ * Fun: hash
+ */
+typedef struct shash_table_s
+{
+	unsigned long int size;
+	shash_node_t **array;
+	shash_node_t *shead;
+	shash_node_t *stail;
+} shash_table_t;
+
+shash_table_t *shash_table_create(unsigned long int size);
+int shash_table_set(shash_table_t *ht, const char *key, const char *value);
+char *shash_table_get(const shash_table_t *ht, const char *key);
+void shash_table_print(const shash_table_t *ht);
+void shash_table_print_rev(const shash_table_t *ht);
+void shash_table_delete(shash_table_t *ht);
+
+/**
+ * struct hash_node_s: This structure shows the Node of the hash table
+ * Author: MikiasHailu
+ * Fun: hash
+ */
+typedef struct hash_node_s
+{
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+} hash_node_t;
+
+/**
+ * struct hash_table_s: This structure shows the data structure of the hash table.
+ * Author: MikiasHailu
+ * Fun: hash
+ */
+typedef struct hash_table_s
+{
+	unsigned long int size;
+	hash_node_t **array;
+} hash_table_t;
+
+hash_table_t *hash_table_create(unsigned long int size);
+unsigned long int hash_djb2(const unsigned char *str);
+unsigned long int key_index(const unsigned char *key, unsigned long int size);
+int hash_table_set(hash_table_t *ht, const char *key, const char *value);
+char *hash_table_get(const hash_table_t *ht, const char *key);
+void hash_table_print(const hash_table_t *ht);
+void hash_table_delete(hash_table_t *ht);
+
+/**
+ * struct shash_node_s: THis struncture shows the Node of a sorted hash table
+ * Author: MikiasHailu
+ * Fun: hash
+ */
+typedef struct shash_node_s
+{
+	char *key;
+	char *value;
+	struct shash_node_s *next;
+	struct shash_node_s *sprev;
+	struct shash_node_s *snext;
+} shash_node_t;
+
+#endif
