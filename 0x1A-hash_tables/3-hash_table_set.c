ef86bade9cfe60fdab0a88c576c441a69a86de4c
mikehailu7 <mikehailu7@gmail.com>
2023-07-06 09:12:57 -0700

    hash

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,48 +1,6 @@
 #include "hash_tables.h"
 
 /**
- * hash_table_set: THis function will sets a key to a value in the hash table
- * @value:This charater is for the node value.
- * @key: This character is the key for the node
- * Author: MikiasHailu.
- * Fun: Hash table.
- * Return: This will return 1 if successful.
- */
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
-	hash_node_t *hash_node, *tmp;
-	char *new_value;
-
-	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
-	   key == NULL || strlen(key) == 0 || value == NULL)
-		return (0);
-	index = key_index((const unsigned char *)key, ht->size);
-	tmp = ht->array[index];
-	while (tmp != NULL)
-	{
-		if (strcmp(tmp->key, key) == 0)
-		{
-			new_value = strdup(value);
-			if (new_value == NULL)
-				return (0);
-			free(tmp->value);
-			tmp->value = new_value;
-			return (1);
-		}
-		tmp = tmp->next;
-	}
-	hash_node = make_hash_node(key, value);
-	if (hash_node == NULL)
-		return (0);
-	hash_node->next = ht->array[index];
-	ht->array[index] = hash_node;
-	return (1);
-}
-
-
-
-/**
  * make_hash_node: THis function will creates a new node.
  * @key: This character is the key for the node
  * @value:This charater is for the node value.
@@ -74,3 +32,41 @@ hash_node_t *make_hash_node(const char *key, const char *value)
 	return (node);
 }
 
+/**
+ * hash_table_set: THis function will sets a key to a value in the hash table
+ * @value:This charater is for the node value.
+ * @key: This character is the key for the node
+ * Author: MikiasHailu.
+ * Fun: Hash table.
+ * Return: This will return 1 if successful.
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
+
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+			key == NULL || strlen(key) == 0 || value == NULL)
+		return (0);
+	index = key_index((const unsigned char *)key, ht->size);
+	tmp = ht->array[index];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
+		return (0);
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
