12deadb3654afc160a2070ba6bac1927e680c179
mikehailu7 <mikehailu7@gmail.com>
2023-08-23 00:32:08 -0700

    makefiles

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# Author: Mikias Hailu
+""" This function will represent a measuring function for island parameter """
+def island_perimeter(grid):
+    """ Return the perimiter of an island """
+    height = len(grid)
+    width = len(grid[0])
+    side = 0
+    dim = 0
+    for m in range(height):
+        for n in range(width):
+            if grid[m][n] == 1:
+                dim = dim + 1
+                if (n > 0 and grid[m][n - 1] == 1):
+                    side += 1
+                if (m > 0 and grid[m - 1][n] == 1):
+                    side = side + 1
+    return dim * 4 - side * 2
---
+++ 0x1C-makefiles/main.c
@@ -0,0 +1,12 @@
+#include "m.h"
+/**
+ * main - This is the main entry point of the program.
+ * Return: always true
+ * Author: Mikias Hailu
+ */
+int main(void)
+{
+    print_school();
+    return (EXIT_SUCCESS);
+}
+
