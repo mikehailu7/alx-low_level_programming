e63992d4dff4415b9d5e3b83a11a400bd54c9b85
mikehailu7 <mikehailu7@gmail.com>
2023-08-23 02:33:09 -0700

    makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-# Author: Mikias Hailu
+# Author: MikiasHailu
 """ This function will represent a measuring function for island parameter """
 def island_perimeter(grid):
-    """ Return the perimiter of an island """
+    """ This function will Return the perimiter of an island """
     height = len(grid)
     width = len(grid[0])
     side = 0
@@ -12,7 +12,7 @@ def island_perimeter(grid):
             if grid[m][n] == 1:
                 dim = dim + 1
                 if (n > 0 and grid[m][n - 1] == 1):
-                    side += 1
+                    side = side + 1
                 if (m > 0 and grid[m - 1][n] == 1):
                     side = side + 1
     return dim * 4 - side * 2
