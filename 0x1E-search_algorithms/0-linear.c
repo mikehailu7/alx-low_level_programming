38bdae204a7641dda8243078d54ef47a0c3d71a7
mikehailu7 <mikehailu7@gmail.com>
2023-10-11 00:48:02 -0700

    search argroithms

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,4 +1,7 @@
 #include "search_algos.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
  * linear_search: This will searche for a value in an array of
  * Author: MikiasHailu
@@ -6,15 +9,18 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int m;
-	if (array == NULL)
+	size_t m = 0;
+
+	if (!array || size == 0)
 		return (-1);
 
-	for (m = 0; m < (int)size; m++)
+	while (m < size)
 	{
-		printf("Value checked array[%u] = [%d]\n", m, array[m]);
-		if (value == array[m])
+		printf("Value checked array[%lu] = [%d]\n", m, array[m]);
+		if (array[m] == value)
 			return (m);
+		m++;
 	}
+
 	return (-1);
 }
